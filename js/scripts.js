5d65974776328d71fbf10bbbed3dd5f083537362
Rudi César Comiotto Modena <rudi.modena@gmail.com>
2019-03-15 22:51:44 -0300

    Updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //Run when the page is ready
 $(function () {
     // Open github page of the Movie Trailer Website Project
-    $("#MovieTrailerWebsite").click(function () {
+    $("#movie-trailer-website").click(function () {
         window.open('https://github.com/rccmodena/movie_trailer_website', '_blank');
     });
 
     // Open github page of the Portfolio Site Project
-    $("#PortfolioSite").click(function () {
+    $("#portfolio-site").click(function () {
         window.open('https://github.com/rccmodena/build_a_portfolio_site', '_blank');
     });
 });
